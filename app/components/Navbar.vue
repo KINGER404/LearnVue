9376d3a554c1a05d7b1ba711f8f6acc4b8d585ba
KINGER <oimasterok@gmail.com>
2020-04-01 17:00:19 +0400

    1

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,11 +1,8 @@
 <template>
-    <div class="header">
-        <div class="header__logo">Maгазин</div>
-        <div class="shopbasket header__shopbasket">
+        <button class="shopbasket header__shopbasket">
             <router-link to="/cart" class="shopbasket">Корзина:</router-link>
             <div class="shopbasket__counter">{{ itemsCount }} / {{ totalPrice }} руб</div>
-        </div>
-    </div>
+    </button>
 </template>
 
 <script>
@@ -19,19 +16,6 @@
 </script>
 
 <style scoped>
-    .header {
-        width: 100%;
-        height: 100px;
-        background-color: beige;
-    }
-    .header__logo {
-        width: 20%;
-        background-color: blanchedalmond;
-    }
-    .header__items {
-        width: 60%;
-        background-color: dodgerblue;
-    }
     .header__shopbasket {
         width: 20%;
         background-color: dodgerblue;
